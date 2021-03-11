e8338929b95a48c118da121688f6c692df67f436
unknown <eldinmemisevic@gmail.com>
2021-03-11 08:09:54 +0100

    Dodao novi fail index.js

---
+++ ex02/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    //Only change code below this line
+    /*document.write("this is multi line comment!");
+    document.write("this is multi line comment!");
+    document.write("this is multi line comment!");
+    document.write("this is multi line comment!");
+    document.write("this is multi line comment!");
+    */
+    //Only change code adove this line
+    
+}
+console.log (main());
+module.exports = main;
