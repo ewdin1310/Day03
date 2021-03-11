1e23da7bbd10a69e381b500e543251c1606509b1
unknown <eldinmemisevic@gmail.com>
2021-03-11 08:10:58 +0100

    Dodao novi fail index.js

---
+++ ex04/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    //Only change code below this line
+    var x = 10;
+    var y = x;
+    //Only change code adove this line
+    return [x, y];
+}
+console.log (main());
+module.exports = main;
