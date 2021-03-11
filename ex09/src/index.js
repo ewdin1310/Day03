56e111911d1875337cb31d4be578a1f300f1a07b
unknown <eldinmemisevic@gmail.com>
2021-03-11 08:11:38 +0100

    Dodao novi fail index.js

---
+++ ex09/src/index.js
@@ -0,0 +1,9 @@
+function main(numOne, numTwo) {
+    //Only change code below this line
+    var numOne = 15;
+    var numTwu = 8;
+    //Only change code adove this line
+    return [numOne - numTwo];
+}
+console.log (main(15, 8));
+module.exports = main;
