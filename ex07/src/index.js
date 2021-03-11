1df53a140befd35dfb31adb2ea13cb78aa4b4587
unknown <eldinmemisevic@gmail.com>
2021-03-11 08:11:22 +0100

    Dodao novi fail index.js

---
+++ ex07/src/index.js
@@ -0,0 +1,18 @@
+function main() {
+    //Only change code below this line
+    // Variable declarations
+    var StUdLyCapVar;
+    var properCamelCase;
+    var TitleCaseOver;
+    //Variable assignments
+    StUdLyCapVar = 10;
+    properCamelCase = "A String";
+    TitleCaseOver = 9000
+    //Only change code adove this line
+    return [StUdLyCapVar,
+        properCamelCase
+        TitleCaseOver]
+        
+}
+console.log (main());
+module.exports = main;
